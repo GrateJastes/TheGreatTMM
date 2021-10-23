8c7e784bdf7d71136d7fbb0fc0e37d3814527a84
Nikita Ermilov <ermilov1999@gmail.com>
2021-10-23 18:05:54 +0300

    tmp

---
+++ main.js
@@ -0,0 +1,29 @@
+// const { app, BrowserWindow } = require('electron');
+//
+// // eslint-disable-next-line no-unused-vars
+// let mainWindow;
+//
+// function createWindow() {
+//   mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     autoHideMenuBar: true,
+//     webPreferences: {
+//       nodeIntegration: true,
+//     },
+//   });
+//
+//   mainWindow.loadURL('http://localhost:8000/main.html');
+//   mainWindow.webContents.openDevTools();
+//
+//   mainWindow.on('closed', () => mainWindow = null);
+// }
+//
+// app
+//   .on('ready', createWindow)
+//   .on('window-all-closed', () => app.quit())
+//   .on('activate', () => {
+//     if (mainWindow === null) createWindow();
+//   });
+
+
