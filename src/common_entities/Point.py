6d0c86061851a8c595d4eb97fb827582abe7b4d4
Sarak <regina10000000000@gmail.com>
2021-10-31 16:15:29 +0300

    added interpolated_path & сhanged diff consts for real tests

--- src/common_entities/Point.py
+++ src/common_entities/Point.py
@@ -1,10 +1,6 @@
-import math
-
-import numpy as np
-
-from .Path import Path
-from src.diff import diff_utils
 from src.diff import consts as diff_consts
+from src.diff import diff_utils
+from .Path import Path
 
 
 class Point:
@@ -21,3 +17,7 @@ class Point:
 
     def analog_angle(self):
         return [i * diff_consts.STEP_SPLITTING for i in range(len(self.speed))]
+
+    # returns a path object - [dot, dot, dot, ...] for an interpolated path
+    def interpolated_path(self, base):
+        return diff_utils.interpolate(self, base).path
