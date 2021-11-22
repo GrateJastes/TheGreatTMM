53d5f72df0663f223ba43bc09f2b9aeabea1f91a
Sarak <regina10000000000@gmail.com>
2021-11-22 17:49:39 +0300

    polynomial tests

--- src/diff/diff_utils.py
+++ src/diff/diff_utils.py
@@ -8,13 +8,23 @@ from src import common_entities
 from src.common_entities.Unit import Unit
 from src.diff import consts
 
+from sympy import lambdify
+from sympy.abc import u
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import interpolate
+from sympy import interpolating_spline
+from scipy.misc import derivative
+
 
 def crop_path(point, base):
     x = [dot.x for dot in point.path.dots]
     y = [dot.y for dot in point.path.dots]
     base_x = [dot.x for dot in base.path.dots]
     base_y = [dot.y for dot in base.path.dots]
-    i = 20
+    base_omega = [dot.omega for dot in base.path.dots]
+    i = consts.NUM_OF_STEPS_FROM_START
     while ((i != len(base_x))
            and ((base_x[i] <= base_x[0] - consts.SHOOTING_ERROR)
                 or (base_x[i] >= base_x[0] + consts.SHOOTING_ERROR)
@@ -25,19 +35,26 @@ def crop_path(point, base):
     y = y[:(i + 1)]
     base_x = base_x[:(i + 1)]
     base_y = base_y[:(i + 1)]
-    return x, y, base_x, base_y
+    base_omega = base_omega[:(i + 1)]
+    return x, y, base_omega
 
 
 def path_smoothing(point, base):
+    if point.name == base.name:
+        smoothing_order = consts.BASE_SMOOTH_ORDER
+    else:
+        smoothing_order = consts.POINT_SMOOTH_ORDER
     ip_point = common_entities.Point(point.name + '(ip)')
     dot_list = []
     point.remove_dot_repeat()
     base.remove_dot_repeat()
-    x, y, base_x, base_y = crop_path(point, base)
+    x, y, base_omega = crop_path(point, base)
+    omega, subject = sort_addiction(base_omega, x)
+    coords_poly(omega,subject)
     x.append(x[0])
     y.append(y[0])
     xy = [x, y]
-    tck_x, ux = splprep(xy, s=2000, per=True)
+    tck_x, ux = splprep(xy, s=smoothing_order, per=True)
     new_points = splev(ux, tck_x)
     i = 0
     while i < len(new_points[0]):
@@ -96,7 +113,6 @@ def diff1(point, base):
                            central_difference_1(ip_dot_list, i, 'y')))
     v_list.append(Unit(extreme_difference_1(ip_dot_list, 'x'), extreme_difference_1(ip_dot_list, 'y')))
     v_list_pol = data_fit(v_list)
-    # v_list_pol = data_interpolate(v_list)
     # v_list_pol = v_list
     return v_list_pol
 
@@ -142,7 +158,6 @@ def diff2(point, base):
         a_list.append(Unit(central_difference_2(ip_dot_list, num, 'x'), central_difference_2(ip_dot_list, num, 'y')))
     a_list.append(Unit(extreme_difference_2(ip_dot_list, 'x'), extreme_difference_2(ip_dot_list, 'y')))
     a_list_pol = data_fit(a_list)
-    # a_list_pol = data_interpolate(a_list)
     # a_list_pol = a_list
     return a_list_pol
 
@@ -175,15 +190,64 @@ def data_fit(data_list):
     return data_list_pol
 
 
-# Another way to smooth the resulting graphs (in progress)
-def data_interpolate(data_list):
-    omega_list = [item * consts.STEP_SPLITTING for item in range(len(data_list))]
-    omega_int = np.linspace(omega_list[0], omega_list[-1], 30)
-    tck_x = interpolate.splrep(omega_list, [unit.x for unit in data_list], k=3, s=1120)
-    int_x = interpolate.splev(omega_int, tck_x, der=0)
-    tck_y = interpolate.splrep(omega_list, [unit.y for unit in data_list], k=3, s=1120)
-    int_y = interpolate.splev(omega_int, tck_y, der=0)
-    data_list_pol = []
-    for i in range(len(data_list)):
-        data_list_pol.append(Unit(int_x[i], int_y[i]))
-    return data_list_pol
+def set_angle_addiction(point, base):
+    point.remove_dot_repeat()
+    base.remove_dot_repeat()
+    x, y, base_omega = crop_path(point, base)
+    x.append(x[0])
+    y.append(y[0])
+    print(len(x), x)
+    print(len(y), y)
+    print(len(base_omega), base_omega)
+    base_omega.append(base_omega[0])
+    # Формат вывода уточнить
+    return [base_omega, x], [base_omega, y]
+
+
+def sort_addiction(omega, subject):
+    points = zip(omega, subject)
+    points = sorted(points, key=lambda point: point[0])
+    return zip(*points) # (base_omega, subject)
+
+def coords_poly(omega, subject):
+    plt.plot(omega, subject)
+    print(omega[0], omega[-1])
+    plt.show()
+    f = polinom(omega, subject)
+    us = np.linspace(- np.pi, np.pi, 100)
+    temp = []
+    for i in us:
+        temp.append(derivative(f, i, dx=1e-6))
+    print('5')
+    plt.plot(us, temp)
+    plt.show()
+    temp2 = []
+    for i in us:
+        temp2.append(derivative(f, i, dx=1e-6, n=2))
+    print('5')
+    plt.plot(us, temp2)
+    plt.show()
+    return temp
+
+
+def coords_smoothing(subject, omega):
+    x = np.r_[omega, omega[0]]
+    y = np.r_[subject, subject[0]]
+    tck, u_ticks = interpolate.splprep([x, y], s=4000, per=True)
+    xi, yi = interpolate.splev(np.linspace(0, 1, 1000), tck)
+    us = [u_ticks[-3] - 1, u_ticks[-2] - 1, *u_ticks, u_ticks[1] + 1, u_ticks[2] + 1]
+    xs = [*x[-3:-1], *x, *x[1:3]]
+    ys = [*y[-3:-1], *y, *y[1:3]]
+    interpx = interpolating_spline(tck[2], u, us, xs)
+    interpy = interpolating_spline(tck[2], u, us, ys)
+    fx = lambdify(u, interpx, modules=['numpy'])
+    fy = lambdify(u, interpy, modules=['numpy'])
+    us = np.linspace(0, 1, 100)
+    plt.scatter(fx(us), fy(us), c=us, s=40, marker='o', cmap='tab10')  # label='sympy´s interpolating_spline'
+    temp = []
+    for i in us:
+        temp.append(derivative(fx, i, dx=1e-6))
+    plt.show()
+    print('5')
+    plt.plot(us, temp)
+    plt.show()
